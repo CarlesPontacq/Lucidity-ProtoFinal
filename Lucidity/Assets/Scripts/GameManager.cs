c77d6eb75aed00767a4ef17927c85e388d83254e
Erica Baltar <erica.baltar@estudiant.enti.cat>
2026-02-25 10:54:56 +0100

    Enemy now avoids walls (player cant die)

--- Lucidity/Assets/Scripts/GameManager.cs
+++ Lucidity/Assets/Scripts/GameManager.cs
@@ -216,6 +216,10 @@ public class GameManager : MonoBehaviour
 
         SetPlayerControlEnabled(true);
 
+        var deathTouch = PlayerRef.GetComponentInChildren<PlayerDeathOnEnemyTouch>(true);
+        if (deathTouch != null)
+            deathTouch.ResetDeathTrigger();
+
         isDying = false;
     }
 

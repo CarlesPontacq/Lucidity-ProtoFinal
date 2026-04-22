9ca0f4dbac767e7a348d8b786b23c728976819a1
CarlesPontacq <carles.pontacq@enti.cat>
2026-04-22 13:31:16 +0200

    Implemented final door

--- Lucidity/Assets/Scripts/Enemy/EnemySpawnerEnabler.cs
+++ Lucidity/Assets/Scripts/Enemy/EnemySpawnerEnabler.cs
@@ -5,12 +5,16 @@ public class EnemySpawnerEnabler : MonoBehaviour
     [SerializeField] private EnemyChaseSpawner enemyChaseSpawner;
     private string playerTag = "Player";
     [SerializeField] private bool enableEnemySpawner;
+    [SerializeField] private DoorInteraction  door;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == playerTag && enemyChaseSpawner.enabled != enableEnemySpawner)
         {
             enemyChaseSpawner.enabled = enableEnemySpawner;
+
+            if(door != null)
+                door.Lock();
         }
     }
 }

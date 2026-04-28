c0eee442a1889519b0d939342b1058c8fcaf4daa
CarlesPontacq <carles.pontacq@estudiant.enti.cat>
2026-04-28 17:41:53 +0200

    Added grab Report, camera and flash sounds

--- Lucidity/Assets/Scripts/Interactables/ReportSheetObject.cs
+++ Lucidity/Assets/Scripts/Interactables/ReportSheetObject.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 public class ReportSheetObject : ObjectInteraction
 {
     [SerializeField] ItemData itemData;
+    [SerializeField] private string grabReportSFX = "";
+    private float grabReportVolumeSFX = 1f;
 
     public override void Interact()
     {
+        SFXManager.Instance.PlayGlobalSound(grabReportSFX, grabReportVolumeSFX);
         GameManager.Instance.ReportSheetGrabbed(itemData);
         Destroy(gameObject);
     }
--- Lucidity/Assets/Scripts/Interactables/StunModeUnlockerObject.cs
+++ Lucidity/Assets/Scripts/Interactables/StunModeUnlockerObject.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class StunModeUnlockerObject : ObjectInteraction
 {
     [SerializeField] ItemData itemData;
-
+    [SerializeField] private string grabFlashSFX = "GrabFlash";
+    private float grabFlashVolumeSFX = 1f;
     public override void Interact()
     {
+        SFXManager.Instance.PlayGlobalSound(grabFlashSFX, grabFlashVolumeSFX);
         GameManager.Instance.StunModeUnlockerGrabbed(itemData);
         Destroy(gameObject);
     }

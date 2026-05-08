fe1161b1efb467a7d997193fd219378f557bcfeb
CarlesPontacq <carles.pontacq@estudiant.enti.cat>
2026-05-08 15:01:09 +0200

    Deleted enemy rigidbody

--- Lucidity/Assets/Scripts/Enemy/NavMesh/NavMovement.cs
+++ Lucidity/Assets/Scripts/Enemy/NavMesh/NavMovement.cs
@@ -6,11 +6,19 @@ public class NavMovement : MonoBehaviour
     [SerializeField] public NavMeshAgent agent;
     private Vector3 playerPosition;
 
+    void Start()
+    {
+        //Solucion Opcion 2: agent.updatePosition = false; agent.updateRotation = false;
+    }
+
     void Update()
     {
         if (GameManager.PlayerRef == null) return;
 
         playerPosition = GameManager.PlayerRef.transform.position;
         agent.SetDestination(playerPosition);
+        Debug.Log("NavMeshAgent velocity: " + agent.velocity.magnitude);
+
+        // Solucion Opcion 1: agent.velocity = agent.desiredVelocity.normalized * agent.speed;
     }
 }

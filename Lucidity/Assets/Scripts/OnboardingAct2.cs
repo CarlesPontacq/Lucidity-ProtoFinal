b10aff49328a4d9d8a6d4ff98d5d749f71016f03
JoanaMorenoC <juana.moreno@enti.cat>
2026-05-12 03:38:37 +0200

    Keep doors closed until stun upgrade grabbed

--- Lucidity/Assets/Scripts/OnboardingAct2.cs
+++ Lucidity/Assets/Scripts/OnboardingAct2.cs
@@ -1,15 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class OnboardingAct2 : MonoBehaviour
 {
     [SerializeField] GameObject flashObjectPrefab;
     [SerializeField] LoopManager loopManager;
+    [SerializeField] float doorsOpeningDelay = 0.5f;
+    [SerializeField] List<DoorInteraction> doorsToOpen;
 
     private bool objectSpawned = false;
 
     private void Start()
     {
         LoopManager.OnLoopStarted += HandleObjectSpawning;
+        GameManager.Instance.OnStunUnlocked += HandleStunUnlocked;
     }
 
     private void HandleObjectSpawning(int loopIndex)
@@ -24,5 +31,25 @@ public class OnboardingAct2 : MonoBehaviour
     {
         Instantiate(flashObjectPrefab);
         objectSpawned = true;
+    }
+
+    private void HandleStunUnlocked()
+    {
+        StartCoroutine(OpenDoorsWithDelay());
+    }
+
+    private IEnumerator OpenDoorsWithDelay()
+    {
+        yield return new WaitForSeconds(doorsOpeningDelay);
+
+        foreach (DoorInteraction door in doorsToOpen)
+        {
+            door.Open(true);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.Instance.OnStunUnlocked += HandleStunUnlocked;
     }
 }
